20c2240cfd9af900e9eff60ff80de40dfd53e1fd
Alexander Moiseyev <daklaktakpak@yandex.ru>
2022-03-22 00:23:34 +0500

    add router basics

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import './menu-item.styles.scss'
+import {withRouter} from "../../HOC/withRouter";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div style={{
-      backgroundImage: `url(${imageUrl})`
-    }} className='background-image'/>
-    <div className='content'>
-      <h1 className='title'>{ title.toUpperCase() }</h1>
-      <span className='subtitle'>Shop now</span>
+const MenuItem = ({ title, imageUrl, size, router, linkUrl }) => {
+  const {location, navigate} = router
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`${location.pathname}${linkUrl}`)}>
+      <div style={{
+        backgroundImage: `url(${imageUrl})`
+      }} className='background-image'/>
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>Shop now</span>
+      </div>
     </div>
-  </div>
-)
+  )
 
-export default MenuItem
+}
+
+
+export default withRouter(MenuItem)
