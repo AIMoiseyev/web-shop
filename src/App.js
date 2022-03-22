e87162eca678d79259b6be8469bf16186a2d5096
Alexander Moiseyev <daklaktakpak@yandex.ru>
2022-03-22 23:06:21 +0500

    Add collections components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-// import {Route} from "react-router-dom";
+import ShopPage from './pages/shop/shop.component';
+import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
