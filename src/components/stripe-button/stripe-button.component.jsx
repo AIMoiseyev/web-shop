1f956c21e2f5dd4e9e7f6952b98800af1171e052
Alexander Moiseyev <daklaktakpak@yandex.ru>
2022-03-31 00:05:36 +0500

    updating stripe, favicon, title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51Kj655FLdk9AtawoGqBV5ojRUVOYDoGdbrbZBF4FtpoKNXDFCNox2TbWyp4X4ooiVBZO7lUWShu7q9rZh90trDoY005FL5bJ6A';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Web Shop"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
