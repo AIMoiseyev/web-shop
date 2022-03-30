1eb4b96748b2dbca1a21c3c507b2bb784d675dfa
Alexander Moiseyev <daklaktakpak@yandex.ru>
2022-03-30 23:05:34 +0500

    make directory reducer, refactor components, add new routes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-class ShopPage extends React.Component {
-  constructor() {
-    super();
+import { Route, Routes } from 'react-router-dom';
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+const ShopPage = () => (
+  <div className="shop-page">
+    <Routes>
+      <Route path="/" element={<CollectionsOverview />} />
+      <Route path={`/:collectionId`} element={<CollectionPage />} />
+    </Routes>
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
 export default ShopPage;
